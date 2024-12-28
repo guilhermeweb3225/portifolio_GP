ade722436c546f3e420243113d0c6c980b4ae83d
Guilherme P.Oliveira <guilhermeponciano3225@gmail.com>
2024-12-28 17:02:07 -0300

    Corrigir base no vite.config.js

--- portifolio-GP/vite.config.js
+++ portifolio-GP/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/portifolio-GP/', // Nome do seu repositório no GitHub
+  base: '/portifolio_GP/', // Nome do seu repositório no GitHub
 })
